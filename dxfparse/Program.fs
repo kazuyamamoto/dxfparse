88803c66b7e61ff40b686fde59b76fbe1df1b38d
yamamotokzj <kazu@rao.co.jp>
2023-06-06 20:46:19 +0900

    Rename readLines into ReadLines

--- dxfparse/Program.fs
+++ dxfparse/Program.fs
@@ -1,7 +1,6 @@
-﻿open System
-open System.IO
+﻿open System.IO
 
-let readLines (file: string) =
+let ReadLines (file: string) =
     seq {
         use reader = new StreamReader(file)
 
@@ -9,4 +8,8 @@ let readLines (file: string) =
             yield reader.ReadLine()
     }
 
-"sample.dxf" |> readLines |> Seq.iter (fun line -> printfn $"{line}")
+"sample.dxf"
+|> ReadLines
+// いつ自動フォーマットで改行するのかを確認するための無意味な処理（id は　入出力が同じことを表す関数）
+|> Seq.map id
+|> Seq.iter (fun line -> printfn $"{line}")

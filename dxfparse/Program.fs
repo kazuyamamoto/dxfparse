6f3da2f43e1a47130e41de771a4d44c1ff295a9c
Kazuhiro Yamamoto <kazuhiro.yamamoto@tokai-soft.co.jp>
2023-06-07 16:32:09 +0900

    Make a sequence of two lines pair

--- dxfparse/Program.fs
+++ dxfparse/Program.fs
@@ -1,5 +1,6 @@
 ﻿open System.IO
 
+// ファイルを読み1行ずつ返す。
 let ReadLines (file: string) =
     seq {
         use reader = new StreamReader(file)
@@ -8,8 +9,17 @@ let ReadLines (file: string) =
             yield reader.ReadLine()
     }
 
+// 2行をペアとしたシーケンスを得る。
+let LinePairs lines =
+    lines
+    |> Seq.indexed 
+    |> Seq.groupBy (fun (index, _) -> index / 2)
+    |> Seq.map snd
+    |> Seq.map(fun seq -> (Seq.head seq, Seq.last seq))
+    |> Seq.map(fun ((_, l1), (_, l2)) -> (l1, l2))
+
 "sample.dxf"
 |> ReadLines
-// いつ自動フォーマットで改行するのかを確認するための無意味な処理（id は　入出力が同じことを表す関数）
+|> LinePairs
 |> Seq.map id
-|> Seq.iter (fun line -> printfn $"{line}")
+|> Seq.iter (fun pair -> printfn $"{pair}")
